d9e567ed9eec8cb19710344491b0088ad5418f94
tory1103 <adrian.rubiboy11@gmail.com>
2022-12-01 22:44:47 +0100

    Actualiza memsym.c: 	- Arregla la direccion de memoria de LimpiarCACHE 	- Añade la opcion de guardar el texto leido en un array 	- Actualiza el output por consola 	- Actualiza la deteccion de existencia de los ficheros

--- memsym.c
+++ memsym.c
@@ -6,10 +6,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #define NUM_FILAS 8
 #define TAM_LINEA 16
-#define TAM_CADENA 100
+#define TAM_RESULTADO 100
 #define TAM_CACHE 256
 #define TAM_RAM 4096
 #define LRAM 1024
@@ -25,14 +26,14 @@ void LimpiarCACHE(T_CACHE_LINE tbl[NUM_FILAS])
 {
 	/* Funcion que limpia la memoria cache, es decir, inicializa los valores de la cache a default */
 	/* - Etiqueta: 0xFF */
-	/* - Datos:0x23F */
+	/* - Datos: 0x23F */
 
 	for (int i=0; i<NUM_FILAS; i++) // Inicializacion de la memoria cache
 	{
 		tbl[i].ETQ = 0xFF; // Todas las etiquetas a 0xFF
 
 		for (int j=0; j<TAM_LINEA; j++)
-			tbl[i].Data[j] = (char) 0x23F; // Todos los datos de las lineas de la memoria cache a 0x23F
+			tbl[i].Data[j] = 0x23; // Todos los datos de las lineas de la memoria cache a 0x23F
 	}
 }
 
@@ -46,6 +47,8 @@ void VolcarCACHE(T_CACHE_LINE *tbl)
 		for (int j=0; j<TAM_LINEA; j++) printf("%02X ",tbl[i].Data[j]);
 		printf("\n");
 	}
+
+	printf("\n");
 }
 
 void ParsearDireccion(unsigned int addr, int *ETQ, int *palabra, int *linea, int *bloque)
@@ -62,7 +65,7 @@ void ParsearDireccion(unsigned int addr, int *ETQ, int *palabra, int *linea, int
 	*bloque = (addr & 0b111111110000) >> 4;
 }
 
-void TratarFallo(T_CACHE_LINE *tbl, char *MRAM, int ETQ, int linea, int bloque)
+void TratarFallo(T_CACHE_LINE *tbl, unsigned char *MRAM, int ETQ, int linea, int bloque)
 {
 	/* Funcion que carga los datos de la MRAM */
 
@@ -81,7 +84,8 @@ int main(int argc, char **argv)
 	// Inicializacion de variables
 	int globalTime = 0, // Tiempo total
 		numeroFallos = 0, // Numero de fallos de la cache
-		numeroAccesos = 0; // Numero de accesos de la cache
+		numeroAccesos = 0, // Numero de accesos de la cache
+		numeroResultado = 0; // Contador de los resultados
 
 	int addr = 0, // Direccion de memoria
 		ETQ = 0, // Etiqueta de la cache
@@ -90,7 +94,8 @@ int main(int argc, char **argv)
 		bloque = 0; // Bloque de la cache (ETQ + linea)
 
 	unsigned char memoriaRAM[TAM_RAM], // Memoria RAM
-				  contenidoCache[TAM_CACHE]; // Contenido cache
+				  contenidoCache[TAM_CACHE], // Contenido cache
+				  resultado[TAM_RESULTADO]; // Contenido valores resultantes
 
 	T_CACHE_LINE memoriaCache[NUM_FILAS]; // Memoria cache
 
@@ -103,13 +108,13 @@ int main(int argc, char **argv)
 
 	if (!contentsRam || !contentsDir) // Comprueba si existen los ficheros
 	{
-		printf(!contentsRam ? "No existe el fichero CONTENTS_RAM.bin\n" : "No existe el fichero accesos_memoria.txt\n");
+		printf(!contentsRam && !contentsDir ? "No existen los ficheros\n" : !contentsRam ? "No existe el fichero CONTENTS_RAM.bin\n" : "No existe el fichero accesos_memoria.txt\n");
 		return -1;
 	}
 
 	// Lectura de los contenidos
 	fread(memoriaRAM, TAM_RAM, 1, contentsRam); // Guardar el .bin en memoria RAM (4096 caracteres)
-	
+
 	while (fscanf(contentsDir, "%x", &addr) != EOF) // Guardar la primera linea del .txt en direccion (numero hexadecimal)
 	{
 		ParsearDireccion(addr, &ETQ, &palabra, &linea, &bloque);
@@ -118,17 +123,25 @@ int main(int argc, char **argv)
 		// Comparamos las etiquetas
 		if (ETQ != memoriaCache[linea].ETQ)
 		{
-			printf("T: %d, Fallo cache: %d, ADDR: %04X, ETQ: %X, Linea: %02X, Palabra: %02X, Bloque: %02X\n", globalTime, ++numeroFallos, addr, ETQ, linea, palabra, bloque);
+			printf("T: %d Fallo cache: %d => ADDR: %04X ETQ: %X Linea: %02X Palabra: %02X Bloque: %02X\n", globalTime, ++numeroFallos, addr, ETQ, linea, palabra, bloque);
 			globalTime += 10;
 
 			TratarFallo(memoriaCache, memoriaRAM, ETQ, linea, bloque);
 		}
 
 		numeroAccesos++;
-		printf("T: %d, Acierto cache ADDR: %04X, ETQ: %X, Linea: %02X, Palabra: %02X, Datos: %02X\n", globalTime, addr, ETQ, linea, palabra, memoriaCache[linea].Data[15 - palabra]);
+		printf("T: %d Acierto cache => ADDR: %04X ETQ: %X Linea: %02X Palabra: %02X Datos: %02X\n", globalTime, addr, ETQ, linea, palabra, memoriaCache[linea].Data[15 - palabra]);
+
+		resultado[numeroResultado++] = memoriaCache[linea].Data[15 - palabra];
 
 		VolcarCACHE(memoriaCache);
+
+		sleep(1);
 	}
+
+	resultado[numeroResultado] = '\0';
+
+	printf("Accesos totales: %d ; Fallos totales: %d ; Tiempo medio: %lf\nTexto leido: %s\n", numeroAccesos, numeroFallos, (double) globalTime / numeroAccesos, resultado);
 
 	// Cerrar punteros de ficheros
 	fclose(contentsRam); // Cerrar el .bin
